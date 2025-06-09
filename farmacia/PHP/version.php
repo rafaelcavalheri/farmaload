48d8a9f775e31cc93c415eaa63ea60882fa8cd1a
rafaelcavalheri <144138270+rafaelcavalheri@users.noreply.github.com>
2025-06-09 13:44:19 -0300

    v.1.2025.0506.1700

--- farmacia/PHP/version.php
+++ farmacia/PHP/version.php
@@ -1,5 +1,5 @@
 <?php
 // Arquivo de versão do sistema
-define('SYSTEM_VERSION', 'v.1.2025.0506.1549');
+define('SYSTEM_VERSION', 'v.1.2025.0506.1700');
 define('SYSTEM_VERSION_DATE', '05/06/2025');
 ?> 
