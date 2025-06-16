32e7998752203d8a4e0cc8f03e7c20581a345a5d
rafaelcavalheri <144138270+rafaelcavalheri@users.noreply.github.com>
2025-06-16 14:08:05 -0300

    ldap-config

--- farmacia/PHP/ldap_settings.php
+++ farmacia/PHP/ldap_settings.php
@@ -1,6 +1,4 @@
 <?php
-return array (
-  'ldap_server' => 'ldap://192.168.10.224',
-  'ldap_domain' => 'mmirim.local',
-  'ldap_base_dn' => 'dc=mmirim,dc=local',
-);
+$ldapServer = 'ldap://192.168.10.224';
+$ldapDomain = 'mmirim.local';
+$ldapBaseDn = 'dc=mmirim,dc=local';
